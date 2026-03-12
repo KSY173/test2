f5ff619cffb37649b023eac1449fac0ba0e6945e
KSY173 <112866329+KSY173@users.noreply.github.com>
2026-03-12 15:28:06 +0900

    [Silver IV] Title: solved.ac, Time: 164 ms, Memory: 37164 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+if n == 0:
+    print(0)
+else:
+    arr = [int(input()) for _ in range(n)]
+    arr.sort()
+
+    cut = int(n * 0.15 + 0.5)
+
+    trimmed = arr[cut:n-cut]
+    avg = sum(trimmed) / len(trimmed)
+
+    print(int(avg + 0.5))
